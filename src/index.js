0f33a75f75ae1bf6bd85a532750688db3cd017a2
neerajjaiswall <neerajjaiswal8952@gmail.com>
2021-05-18 08:57:19 +0530

    initiated react app
    react app initial setup.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Home!</h1>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
