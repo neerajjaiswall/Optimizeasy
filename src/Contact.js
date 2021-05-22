0398d5127c7edb783d0e6f7752a2939c7a55260d
neerajjaiswall <neerajjaiswal8952@gmail.com>
2021-05-22 08:46:25 +0530

    Added contact page
    Added simple contact page.

--- src/Contact.js
+++ src/Contact.js
@@ -3,7 +3,34 @@ import React from 'react'
 const Contact = () => {
     return (
         <>
-        <h1> Welcome Contact</h1>
+        <div id="header" className = "my-5 align-items-center">
+            <h1 className = "text-center"> Contact Us </h1>
+        </div>
+        <div className = "container contact_div">
+            <div className = "row">
+                <div className = "col-md-6 col-10 mx-auto">
+                    <form>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1">Full Name</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder=""/>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1">Phone</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder=""/>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1">Message</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary mb-2">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
         </>
     );
 };
