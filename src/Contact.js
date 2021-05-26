c87dee356dfe3f51f83d3785cd7b3470d4f5e780
YashTayal04 <yashtayal341@gmail.com>
2021-05-26 17:22:12 +0530

    Changed home and contact page

--- src/Contact.js
+++ src/Contact.js
@@ -1,38 +1,30 @@
-import React from 'react'
+import React from "react";
+import {
+  Box,
+  Heading,
+  Text,
+  Input,
+  Textarea,
+  Center,
+  Button,
+} from "@chakra-ui/react";
 
 const Contact = () => {
-    return (
-        <>
-        <div id="header" className = "my-5 align-items-center">
-            <h1 className = "text-center"> Contact Us </h1>
-        </div>
-        <div className = "container contact_div">
-            <div className = "row">
-                <div className = "col-md-6 col-10 mx-auto">
-                    <form>
-                    <div class="mb-3">
-                        <label for="exampleFormControlInput1">Full Name</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder=""/>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleFormControlInput1">Phone</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder=""/>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleFormControlInput1">Email address</label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleFormControlTextarea1">Message</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary mb-2">Submit</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-        </>
-    );
+  return (
+    <Center>
+      <Box w="50%" my={20}>
+        <Heading fontSize={20} textAlign="center" my={2}>
+          Drop out your queries. We will reach out to you soon.
+        </Heading>
+        <Input my={2} placeholder="Name" />
+        <Input my={2} placeholder="Email" />
+        <Input my={2} placeholder="Mobile Number" />
+        <Textarea my={2} placeholder="Enter your message" />
+
+        <Button>Submit</Button>
+      </Box>
+    </Center>
+  );
 };
 
 export default Contact;
--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,60 @@
 import React from "react";
-import web from "../src/images/web.png";
-import Common from "./Common";
+import { Box, Heading, Text, Image } from "@chakra-ui/react";
+import home from "../src/images/home.png";
+import learn from "../src/images/learn.png";
 
 const Home = () => {
   return (
-    <>
-        <Common
-        para1="Learn and Grow with"
-        para2="We are proudly teaching competitive programming topics with
-        experts and empowering the world."
-        imgsrc={web}
-        btname="Get Started"
-        visit="/contact"
-      />
-    </>
+    <Box backgroundColor="#F3FCF8">
+      <Box display={{ md: "flex" }} px={10}>
+        <Box w={{ md: "1/2" }}>
+          <Box py="25%" px="10%">
+            <Text fontSize={40}>Learn and Grow with </Text>
+            <Text fontSize={60} color="#333333">
+              CP Portal
+            </Text>
+            <Text fontSize={20}>
+              We are proudly teaching competitive programming topics with
+              experts and empowering the world.{" "}
+            </Text>
+          </Box>
+        </Box>
+        <Box>
+          <Image src={home} alt="image" />
+        </Box>
+      </Box>
+      <Box display={{ md: "flex" }} py={20} px={10}>
+        <Box>
+          <Image src={learn} alt="image" />
+        </Box>
+        <Box w={{ md: "1/2" }}>
+          <Box py="10%" px="10%">
+            <Heading>Why Learn Here</Heading>
+            <Box backgroundColor="white" my={5} px={2}>
+              <Text fontSize={30}>Low Cost</Text>
+              <Text fontSize={20}>
+                We provide course at very low price comparable to other online
+                courses.{" "}
+              </Text>
+            </Box>
+            <Box backgroundColor="white" my={5} px={2}>
+              <Text fontSize={30}>Best Instructor</Text>
+              <Text fontSize={20}>
+                Instructors of our courses are best programmer in world with
+                lots of experience.
+              </Text>
+            </Box>
+            <Box backgroundColor="white" my={5} px={2}>
+              <Text fontSize={30}>Learn topic wise topic</Text>
+              <Text fontSize={20}>
+                Our courses are dedicated over a single topic so that you can
+                study different topics with different instructors.
+              </Text>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
